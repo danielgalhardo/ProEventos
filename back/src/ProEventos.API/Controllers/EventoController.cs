82f95c4ab4f552b3c9a2ee421525671781163444
Daniel <galhardo@ice.ufjf.br>
2021-07-26 14:27:26 -0300

    Seção 3: Introdução ao EF Core

--- back/src/ProEventos.API/Controllers/EventoController.cs
+++ back/src/ProEventos.API/Controllers/EventoController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ProEventos.API.Models;
+using ProEventos.API.Data;
+
 
 namespace ProEventos.API.Controllers
 {
@@ -13,45 +15,23 @@ namespace ProEventos.API.Controllers
     public class EventoController : ControllerBase
     {
 
-        public IEnumerable<Evento> _evento = new Evento[] {
-               new Evento() {
-                             
-               EventoId = 1,
-               Tema = "Angular 11 e . NET 5",
-               Local = "Além Paraíbaa",
-               Lote = "1º lote",
-               QtdPessoas = 250,
-               DataEvento = DateTime.Now.AddDays(2).ToString(),
-               ImagemURL = "foto.png"
-
-                },
-               new Evento() {
-                             
-               EventoId = 2,
-               Tema = "Angular 11 e suas novidades",
-               Local = "Sao Paulo",
-               Lote = "3º lote",
-               QtdPessoas = 350,
-               DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-               ImagemURL = "fotofeia.png"
-
-                }    
-            };    
-        public EventoController()
+      private readonly DataContext _context;
+        public EventoController(DataContext context)
         {
+           _context = context;
         }
 
         [HttpGet]
         public  IEnumerable<Evento> Get()
         {
-           return _evento; // retorna todos os eventos sem filtro.
+           return _context.Eventos; // retorna todos os eventos sem filtro.
         }
 
 
         [HttpGet("{id}")]
-        public  IEnumerable<Evento> GetById(int id)
+        public  Evento GetById(int id)
         {
-           return _evento.Where(evento => evento.EventoId == id);  // utilizado para pegar apenas o id durante 
+           return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);  // utilizado para pegar apenas o id durante 
                                                                    // o get
         }
 
